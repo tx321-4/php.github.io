6526cf2f3c06e03272c32affef1257a27d63e076
shi_yi <shi_yidashi@163.com>
2019-03-11 14:06:34 +0800

    php 语法

---
+++ php_grammar/01.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>基本的PHP语法</title>
+</head>
+<body>
+    <h1>My first PHP page</h1>
+
+    <?php
+    echo 'Hello World!';
+    ?>
+</body>
+</html>
---
+++ php_grammar/02.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP中的注释</title>
+</head>
+<body>
+    <?php 
+    //这是PHP 单行注释
+
+    /*
+    这是
+    PHP 多行
+    注释
+     */
+    ?>
+</body>
+</html>
