cde81a5132996784c94b4e7d4e8891fa7650e7a2
shi_yi <shi_yidashi@163.com>
2019-03-12 14:24:59 +0800

    php switch

---
+++ php_switch/01.php
@@ -0,0 +1,21 @@
+<title>PHP switch</title>
+<?php
+
+$favcolor = "red";
+switch ($favcolor) {
+    case 'red':
+        echo "你喜欢的颜色是红色！";
+        break;
+    case 'blue':
+        echo "你喜欢的颜色是蓝色！";
+        break;
+    case 'green':
+        echo "你喜欢的颜色是绿色！";
+        break;
+    
+    default:
+        echo "你喜欢的颜色不是 红，蓝，或绿色";
+        break;
+}
+
+?>
