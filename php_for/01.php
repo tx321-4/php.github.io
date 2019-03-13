453e2a58c9e53d37d08c75e9d8bcea3c46c50ac2
shi_yi <shi_yidashi@163.com>
2019-03-13 09:12:23 +0800

    php 循环

---
+++ php_for/01.php
@@ -0,0 +1,9 @@
+<title>PHP for循环</title>
+<?php
+
+for($i=1; $i<=5; $i++)
+{
+    echo "The number is " . $i . "<br />";
+}
+
+?>
---
+++ php_for/02.php
@@ -0,0 +1,10 @@
+<title>PHP foreach循环</title>
+<?php
+
+$x = array('one','two','three');
+foreach($x as $value)
+{
+    echo $value . "<br />";
+}
+
+?>
